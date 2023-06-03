3b9caab8ec90280b29dfb5fd9d36962a70b1e635
sedaliSedalian <seyedalian@outlook.com>
2023-06-03 07:44:16 +0330

    Make MEDIA_ROOT and MEDIA_URL

--- digpro/settings.py
+++ digpro/settings.py
@@ -36,8 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    
+    'django.contrib.staticfiles',   
+
     'products',
     'rest_framework',
 ]
@@ -120,11 +120,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Media Files (for upload_to)
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media Files (for upload_to)
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
