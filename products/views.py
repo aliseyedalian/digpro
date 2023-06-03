968edc27e2be7175a05a8f5be806597bf8bab5f2
sedaliSedalian <seyedalian@outlook.com>
2023-06-03 14:55:34 +0330

    Add APIView for get product(s)

--- products/views.py
+++ products/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+
+class ProductListView(APIView):
+
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True, 
+                                       context = {'request':request})# absolute url for media
+        return Response(serializer.data)
+
+
+class ProductDetailView(APIView):
+
+    def get(self, request, pk):
+        product = Product.objects.get(pk=pk)
+        serializer = ProductSerializer(product, context = {'request':request})
+        return Response(serializer.data)
+
